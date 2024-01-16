98b2b5cdc14db830e089bef2d359668c2f5031eb
user <user@localhost>
2024-01-16 15:30:03 +0500

    Fix

--- iops_procces_check.sh
+++ iops_procces_check.sh
@@ -4,12 +4,10 @@ PROC="$1"
 
 proc_write=$(sudo pidstat -C "$PROC" -d | awk '{s += $5} END {print s}')
 
-#answer="$(curl -Is $DOMAIN | head -1 | awk '{print $2}')"
-
 if [ -z "${proc_write}" ];
 then
     echo 0
     exit
 else
-echo $proc_write
+    echo $proc_write | tr ',' '.'
 fi
